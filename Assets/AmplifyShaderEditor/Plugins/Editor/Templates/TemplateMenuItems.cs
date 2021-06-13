503ee6cbd5fbc968be7a544cbf455d48c3ce9b8c
santi <santi@DESKTOP-SCRENCQ>
2021-06-13 13:20:47 -0300

    Listo

--- Assets/AmplifyShaderEditor/Plugins/Editor/Templates/TemplateMenuItems.cs
+++ Assets/AmplifyShaderEditor/Plugins/Editor/Templates/TemplateMenuItems.cs
@@ -46,15 +46,15 @@ namespace AmplifyShaderEditor
 		{
 			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "e1de45c0d41f68c41b2cc20c8b9c05ef" );
 		}
-		[MenuItem( "Assets/Create/Amplify Shader/Legacy/Samples/DoublePassUnlit", false, 85 )]
+		[MenuItem( "Assets/Create/Amplify Shader/Templates/PostProcessStack", false, 85 )]
 		public static void ApplyTemplate8()
 		{
-			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "003dfa9c16768d048b74f75c088119d8" );
+			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "71afb715c17dc8441903f67851f3c9ec" );
 		}
-		[MenuItem( "Assets/Create/Amplify Shader/Templates/PostProcessStack", false, 85 )]
+		[MenuItem( "Assets/Create/Amplify Shader/Legacy/Samples/DoublePassUnlit", false, 85 )]
 		public static void ApplyTemplate9()
 		{
-			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "71afb715c17dc8441903f67851f3c9ec" );
+			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "003dfa9c16768d048b74f75c088119d8" );
 		}
 	}
 }
